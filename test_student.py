c307453aada4dc98451a53b143967ade03294773
anusankanal <anusankanal@gmail.com>
2025-12-27 12:57:08 +0530

    Update test to use vehicle_details function

--- test_student.py
+++ test_student.py
@@ -1,11 +1,13 @@
-from student.py import student_details
-
+from student import vehicle_details
 def test_student_details():
+    name="Anusha"
+    usn="0E318"
+    div="E"
+    age=20
     expected_output=(
-        "student name=Anusha\n"
-        "student usn=0E318\n"
-        "student div=E\n"
-        "student age=20\n"
+    "student name:Anusha\n"
+    "student usn:0E318\n"
+    "student div:E\n"
+    "student age:20\n"
     )
-    
-    assert student_details("Anusha","0E318","E",20)==expected output
+    assert vehicle_details(name, usn, div,age) == expected_output
